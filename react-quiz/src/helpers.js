b9722f57046ccfe49cda6d80acf91ee5e81bbcfb
m-Cho1 <mturtle051@gmail.com>
2022-11-07 09:56:40 +0000

    used opentdb.com trivia page to create random 10 questions with multipla choice, and fetched the data from API

--- react-quiz/src/helpers.js
+++ react-quiz/src/helpers.js
@@ -9,3 +9,15 @@ export const  shuffleAnswers = (question) => {
     value: unshuffledAnswer
   })).sort((a,b) => a.sort - b.sort).map(a => a.value)
 };
+
+export const normalizeQuestions = (backendQuestions) => {
+  return backendQuestions.map((backendQuestion) => {
+    const incorrectAnswers = backendQuestion.incorrect_answers.map((incorrectAnswer) => decodeURIComponent(incorrectAnswer));
+    return {
+      correctAnswer: decodeURIComponent(backendQuestion.correct_answer),
+      question: decodeURIComponent(backendQuestion.question),
+      incorrectAnswers,
+
+    }
+  })
+}
